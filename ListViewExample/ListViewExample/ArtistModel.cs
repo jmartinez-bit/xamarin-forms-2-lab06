78962ef5fca37076c8b720f2f3cbac29791c3a36
Lincoln Martinez <lincoln.martinez@tecsup.edu.pe>
2021-04-12 15:56:15 -0500

    Agregar archivos de proyecto.

---
+++ ListViewExample/ListViewExample/ArtistModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace ListViewExample
+{
+    public class ArtistModel
+    {
+        public string Band { get; set; }
+        public string Name { get; set; }
+        public string Song { get; set; }
+
+        public ArtistModel()
+        {
+        }
+    }
+
+    public class GroupedArtistModel : ObservableCollection<ArtistModel>
+    {
+        public string LongName { get; set; }
+        public string ShortName { get; set; }
+    }
+}
---
+++ ListViewExample/ListViewExample/ListViewGroup.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.ObjectModel;
+
+namespace ListViewExample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewGroup : ContentPage
+    {
+        private ObservableCollection<GroupedArtistModel> grouped { get; set; }
+        public ListViewGroup()
+        {
+            InitializeComponent();
+			grouped = new ObservableCollection<GroupedArtistModel>();
+			var rock = new GroupedArtistModel() { LongName = "Rock", ShortName = "R" };
+			var pop = new GroupedArtistModel() { LongName = "Pop", ShortName = "P" };
+			var shoegazing = new GroupedArtistModel() { LongName = "Shoegazing", ShortName = "S" };
+			rock.Add(new ArtistModel() { Name = "Eddie Vedder", Band = "Perl Jam" });
+			rock.Add(new ArtistModel() { Name = "Roger Waters", Band = "Pink Floyd" });
+			rock.Add(new ArtistModel() { Name = "Bono", Band = "U2" });
+			pop.Add(new ArtistModel() { Name = "Adam Levine", Band = "Maroon 5" });
+			pop.Add(new ArtistModel() { Name = "Sigrid Raabe", Band = "Sigrid" });
+			pop.Add(new ArtistModel() { Name = "Molly Rankin", Band = "Alvvays" });
+			shoegazing.Add(new ArtistModel() { Name = "Rachel Goswell", Band = "Slowdive" });
+			shoegazing.Add(new ArtistModel() { Name = "Anthony Gonzalez", Band = "M83" });
+			shoegazing.Add(new ArtistModel() { Name = "Johan Duncanson", Band = "The Radio Dept." });
+
+			grouped.Add(rock); grouped.Add(pop); grouped.Add(shoegazing);
+			lstView.ItemsSource = grouped;
+		}
+    }
+}
